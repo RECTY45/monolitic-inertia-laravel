aae8e6d043e2a480c365d6351feb36d98728f71b
RECTY45 <recty.exploit@gmail.com>
2023-08-04 14:33:55 +0800

    feat:make a change

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -19,44 +19,45 @@ export default function Dashboard({ auth, pemetaan }) {
         >
             <Head title="Dashboard" />
 
-            <div className="py-5">
+            <div className="py-3">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                        <div className="p-6 text-gray-900">
-                        <Filter/>
-                        </div>
-                    </div>
-            </div>
-
+                    <div className="p-6 text-gray-900">
+                        <Filter />
             <MapContainer
                 center={[-5.143759051537124, 119.40061707043272]}
                 zoom={5}
                 scrollWheelZoom={false}
+                className="md:w-[1080px] md:h-[500px]"
             >
                 <TileLayer
-                      url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
-                      subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
+                    url="http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}"
+                    subdomains={['mt0', 'mt1', 'mt2', 'mt3']}
                 />
-              
-               {pemetaan && pemetaan.map((item, i) => (
-                <React.Fragment key={i}>
-                    <Marker position={{lat: item.latitude, lng: item.longitude}}>
-                        <Popup  >
-                         <ul>
-                            <li className="text-center">  
-                                <div className="flex justify-center py-5">
-                            <img className="md:w-[50px] md:h-[50px] object-fit-cover rounded-full" src={`http://127.0.0.1:8000/storage/${item.gambar}`} alt=""/>
-                                </div>
-                            </li>
-                            <li className="mb-2">Tempat : {item.lokasi}</li>
-                            <li className="mb-2">Bahan Baku : {item.bahan_baku}</li>
-                            <li className="mb-2">Keterangan : {item.keterangan}</li>
-                         </ul>
-                        </Popup>
-                    </Marker>
-                </React.Fragment>
-               ))}
-              
+
+                {pemetaan && pemetaan.map((item, i) => (
+                    <React.Fragment key={i}>
+                        <Marker position={{ lat: item.latitude, lng: item.longitude }}>
+                            <Popup  >
+                                <ul>
+                                    <li className="text-center">
+                                        <div className="flex justify-center py-5">
+                                            <img className="md:w-[50px] md:h-[50px] object-fit-cover rounded-full" src={`http://127.0.0.1:8000/storage/${item.gambar}`} alt="" />
+                                        </div>
+                                    </li>
+                                    <li className="mb-2">Tempat : {item.lokasi}</li>
+                                    <li className="mb-2">Bahan Baku : {item.bahan_baku}</li>
+                                    <li className="mb-2">Keterangan : {item.keterangan}</li>
+                                </ul>
+                            </Popup>
+                        </Marker>
+                    </React.Fragment>
+                ))}
+
             </MapContainer>
+                    </div>
+                </div>
+            </div>
+
         </AuthenticatedLayout>
     );
 }
